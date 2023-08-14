e0e7e0b83fe2253c4b9fa5f3e08850005d6882b5
Douglas Sánchez <142137936+DouglasSanchezMarchelli@users.noreply.github.com>
2023-08-13 22:43:54 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿//Programa que ingrese el precio de  10 productos, al final del total agregar 13% de IVA//
+String nombreProducto;
+int cantidadxProducto, cantidaDeProductos = 2;
+double precio, total, totalAcumulado = 0;
+
+for (int i = 1; i <= cantidaDeProductos; i++)
+{
+    Console.WriteLine("Nombre del Producto :" + i + ":");
+    nombreProducto = Console.ReadLine();
+    Console.WriteLine("Cantidad Producto " + i + ":");
+    cantidadxProducto = int.Parse(Console.ReadLine());
+    Console.WriteLine("Precio Producto " + i + ":");
+    precio = double.Parse(Console.ReadLine());
+    total = (cantidadxProducto * precio) * 1.13;
+    totalAcumulado = totalAcumulado + total;
+    Console.Clear();
+}
+
+Console.WriteLine("El total a pagar es : " + totalAcumulado);
+
